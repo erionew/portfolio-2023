4a089a7f15c021fff774888fb4eae1c519906a4f
Erione White <erionewhite1999@gmail.com>
2023-04-11 19:39:10 -0500

    mobile nav bar completed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import {React, useState} from 'react'
+import '../stylesheets/nav.css'
 
 export default function Nav({homeLink, skillsLink, projectsLink, contactLink}) {
-  return (
+    const [showMenu, setShowMenu] = useState(false)
+    const handleMenuClick = (e) => {
+        document.querySelector('.mobile--nav__links').classList.toggle('active')
+    }
+    
+    return (
     <nav className='nav--container'>
-      <ul className='nav__ul'>
-        <li className='nav__li'><a href={homeLink} className='nav__link'>Home</a></li>
-        <li className='nav__li'><a href={skillsLink} className='nav__link'>Skills</a></li>
-        <li className='nav__li'><a href={projectsLink} className='nav__link'>Projects</a></li>
-        <li className='nav__li'><a href={contactLink} className='nav__link'>Contact</a></li>
-      </ul>
-      <p>erione white</p>
+      <div className='mobile-nav--container'>
+        <div className='mobile-nav'>
+            <button onClick={handleMenuClick}><i className="las la-bars"></i></button>
+            
+        </div>
+        <ul className='mobile--nav__links nav-links__ul'>
+            <li className='nav-links__li'><a href={homeLink} className='nav-links__link'>Home</a></li>
+            <li className='nav-links__li'><a href={skillsLink} className='nav-links__link'>Skills</a></li>
+            <li className='nav-links__li'><a href={projectsLink} className='nav-links__link'>Projects</a></li>
+            <li className='nav-links__li'><a href={contactLink} className='nav-links__link'>Contact</a></li>
+        </ul>
+      </div>
+
+      <p className='nav__name'>erione white</p>
     </nav>
   )
 }
