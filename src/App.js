e048773313e834cb34456ec27e980b26520a794d
Erione White <erionewhite1999@gmail.com>
2023-04-12 19:35:08 -0500

    fixed nav bg color, installed react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Contact from './components/Contact';
 import Nav from './components/Nav';
+import Home from './components/Home';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container--main">
+
       <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='container--home page'>
+        <h1>My name is <strong>Erione White</strong></h1>
+        <h2><strong>Web Developer</strong></h2>
+    </div>
+  )
+}
