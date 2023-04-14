c12b8ea3372ce10ecb22301b71b8e41e215dd5b7
Erione White <erionewhite1999@gmail.com>
2023-04-14 17:04:02 -0500

    project info will appear on hover, and when clicked

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,19 +1,37 @@
 import React, { useState } from 'react'
 
 export default function Project({img, name, liveLink, repoLink}) {
+
+  const [infoShown, setInforShown] = useState(false)
+
+    const handleClick = (e) => {
+        setInforShown(!infoShown)
+        if(infoShown){
+          if(e.target.className == 'project__info'){
+            e.target.style.opacity = 1
+          }
+        } else {
+          if(e.target.className == 'project__info'){
+            e.target.style.opacity = 0
+          }
+        }
+    }
     function showInfo(e){
+      if(e.target.className == 'project__info'){
         e.target.style.opacity = 1
+      }
     }
-
     function hideInfo(e){
-        e.target.style.opacity = 0
+        if(e.target.className == 'project__info'){
+          e.target.style.opacity = 0
+        }
     }
-
+    
 
   return (
     <div className='container--project'>
       <img className='project__img' src={img}></img>
-      <div onMouseOver={showInfo} onMouseLeave={hideInfo} className='project__info'>
+      <div onClick={handleClick} onMouseOver={showInfo} onMouseLeave={hideInfo}className='project__info'>
         <h2 className='project__name'><strong>{name}</strong></h2>
         <div className='project__link-container'>
             <a href={liveLink} className='project__link_container__link' target="_blank">Live Demo</a>
